94d64a240f7378828383a469e623e15b03e85ee6
TalhaBinShakeel <talharathore00@gmail.com>
2024-04-21 16:28:08 +0500

    Graph&Real

---
+++ lib/values/graph/soil_Ec.dart
@@ -0,0 +1,316 @@
+import 'package:PAVF/values/graph/graphvalue.dart';
+import 'package:PAVF/values/graph/soil_nitrogen.dart';
+import 'package:PAVF/values/real/soilec.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+import 'package:toggle_switch/toggle_switch.dart';
+import 'package:PAVF/component/drawer.dart';
+import 'package:PAVF/values/real/potassium.dart';
+
+import 'package:intl/intl.dart';
+
+// Define the main widget for the real-time screen
+class SoilECgraph extends StatefulWidget {
+  SoilECgraph({Key? key}) : super(key: key);
+
+  @override
+  _SoilgECraphState createState() => _SoilgECraphState();
+}
+
+class _SoilgECraphState extends State<SoilECgraph> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final String user = "To Agro_Farm";
+  final PageController _pageController = PageController();
+
+  final List<String> textData = [
+    "Soil EC",
+  ];
+  final List<IconData> iconData = [
+    Icons.warning, // Temperature icon
+  ];
+  int currentIndex = 0;
+
+  int speedValue1 = 0;
+
+  // Define chart data
+  final List<SalesData> chartData = [
+    SalesData(
+      DateTime(1, 1),
+      10,
+    ),
+    SalesData(DateTime(2, 1), 12),
+    SalesData(DateTime(3, 1), 13),
+    SalesData(DateTime(4, 1), 15),
+  ];
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: _buildAppBar(context),
+      body: _buildBody(),
+      drawer: buildDrawer(context),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      toolbarHeight: 120,
+      centerTitle: true,
+      flexibleSpace: _buildAppBarBackground(),
+      leading: IconButton(
+        icon: Icon(Icons.menu, color: Colors.white, size: 35),
+        onPressed: () {
+          _scaffoldKey.currentState?.openDrawer();
+        },
+      ),
+      title: Text(
+        'Welcome $user', // Display the greeting with the username
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      // actions: [_buildProfileIcon()],
+    );
+  }
+
+  Widget _buildAppBarBackground() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/plant.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Tempgraph()),
+                  );
+                },
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: null,
+                ),
+              ),
+              SizedBox(width: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Icon(
+                  iconData[currentIndex],
+                  size: 40,
+                ),
+              ),
+              Text(
+                textData[currentIndex],
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(width: 0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    //forward move
+                    MaterialPageRoute(builder: (context) => SoilNCgraph()),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios),
+                    onPressed: null,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              children: [
+                _buildTextScreen(0, speedValue1),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextScreen(int index, int speedValue) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 10),
+        ToggleSwitch(
+          minWidth: 90.0,
+          cornerRadius: 100.0,
+          activeBgColors: [
+            [Color(0xFF18A818)],
+            [Color(0xFF18A818)]
+          ],
+          activeFgColor: Color.fromARGB(255, 6, 6, 6),
+          inactiveBgColor: Color(0xFFC9E9C9),
+          inactiveFgColor: Color.fromARGB(255, 0, 0, 0),
+          initialLabelIndex: 1,
+          totalSwitches: 2,
+          labels: ['Graph', 'Realtime'],
+          radiusStyle: true,
+          onToggle: (index) {
+            if (index == 0) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SoilECgraph()),
+              );
+            } else if (index == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SoilEcValue()),
+              );
+            }
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 350,
+            child: Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Card(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _buildIntervalButton('1d'),
+                            _buildIntervalButton('3d'),
+                            _buildIntervalButton('7d'),
+                            _buildIntervalButton('1m'),
+                            _buildIntervalButton('1y'),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            height: 300,
+                            child: SfCartesianChart(
+                              backgroundColor: Colors.white,
+                              primaryXAxis: DateTimeAxis(
+                                title: AxisTitle(
+                                  text: 'Date',
+                                  textStyle: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                majorGridLines: MajorGridLines(
+                                  width: 1,
+                                  color: Colors.grey[300],
+                                ),
+                                plotOffset: 0,
+                              ),
+                              primaryYAxis: NumericAxis(
+                                title: AxisTitle(
+                                  text: 'Value',
+                                  textStyle: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                axisLine: AxisLine(
+                                  width: 1,
+                                  color: Colors.grey[300],
+                                ),
+                                majorTickLines: MajorTickLines(
+                                  size: 5,
+                                  color: Colors.grey[300],
+                                ),
+                                numberFormat: NumberFormat.compact(),
+                              ),
+                              // Remove plotArea here
+                              series: <CartesianSeries>[
+                                LineSeries<SalesData, DateTime>(
+                                  dataSource: chartData,
+                                  xValueMapper: (SalesData sales, _) =>
+                                      sales.year,
+                                  yValueMapper: (SalesData sales, _) =>
+                                      sales.sales,
+                                  color: Colors.blue,
+                                  width: 2,
+                                  markerSettings: MarkerSettings(
+                                    isVisible: true,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ],
+                              tooltipBehavior: TooltipBehavior(
+                                enable: true,
+                                textStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildIntervalButton(String interval) {
+    return ElevatedButton(
+      onPressed: () {
+        // Handle button press for the given interval
+        // Update chart data based on the selected interval
+      },
+      child: Text(interval),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: SoilECgraph(),
+  ));
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+  final DateTime year;
+  final double sales;
+}
---
+++ lib/values/real/soilec.dart
@@ -0,0 +1,223 @@
+import 'package:PAVF/values/graph/graphvalue.dart';
+import 'package:PAVF/values/graph/soil_Ec.dart';
+import 'package:PAVF/values/real/nitrogen.dart';
+import 'package:PAVF/values/real/potassium.dart';
+import 'package:PAVF/values/real/temperature.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kdgaugeview/kdgaugeview.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+import 'package:PAVF/component/drawer.dart';
+
+// Define the main widget for the real-time screen
+class SoilEcValue extends StatefulWidget {
+  SoilEcValue({Key? key}) : super(key: key);
+
+  @override
+  _SoilEcValueState createState() => _SoilEcValueState();
+}
+
+class _SoilEcValueState extends State<SoilEcValue> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final String user = "To Agro_Farm";
+  final PageController _pageController = PageController();
+
+  final List<String> textData = [
+    "Soil EC",
+  ];
+  final List<IconData> iconData = [
+    Icons.thermostat, // Soil Moisture icon
+  ];
+  int currentIndex = 0;
+  // Global variables for each meter's speed value
+  // double speedValue1 =
+  //     double.parse(retrieveData('temp')); // Speed value for Potassium
+  // double speedValue2 =
+  //     double.parse(retrieveData('light')); // Speed value for Nitrogen
+  // double speedValue3 =
+  //     double.parse(retrieveData('light')); // Speed value for Phosphorus
+  // double speedValue4 =
+  //     double.parse(retrieveData('light')); // Speed value for Soil EC
+
+  // Define the speed value for the Potassium meter
+// double temperatureSpeed = double.parse(retrieveData('temp'));
+// double lightSpeed = double.parse(retrieveData('light'));
+// double humiditySpeed  = double.parse(retrieveData('humid'));
+  int speedValue1 = 30;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: _buildAppBar(context),
+      body: _buildBody(),
+      drawer: buildDrawer(context),
+    );
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.black,
+      toolbarHeight: 120,
+      centerTitle: true,
+      flexibleSpace: _buildAppBarBackground(),
+      leading: IconButton(
+        icon: Icon(Icons.menu, color: Colors.white, size: 35),
+        onPressed: () {
+          _scaffoldKey.currentState?.openDrawer();
+        },
+      ),
+      title: Text(
+        'Welcome $user', // Display the greeting with the username
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      // actions: [_buildProfileIcon()],
+    );
+  }
+
+  Widget _buildAppBarBackground() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/plant.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TemperatureValue()),
+                  );
+                },
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: null,
+                ),
+              ),
+              SizedBox(width: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Icon(
+                  iconData[currentIndex],
+                  size: 40,
+                ),
+              ),
+              Text(
+                textData[currentIndex],
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(width: 0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    //forward move
+                    MaterialPageRoute(builder: (context) => Nitrogenvalue()),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios),
+                    onPressed: null,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              children: [
+                _buildTextScreen(0, speedValue1),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextScreen(int index, int speedValue) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 50),
+        ToggleSwitch(
+          minWidth: 90.0,
+          cornerRadius: 100.0,
+          activeBgColors: [
+            [Color(0xFFC9E9C9)],
+            [Color(0xFFC9E9C9)]
+          ],
+          activeFgColor: Color.fromARGB(255, 6, 6, 6),
+          inactiveBgColor: Color(0xFF18A818),
+          inactiveFgColor: Color.fromARGB(255, 0, 0, 0),
+          initialLabelIndex: 1,
+          totalSwitches: 2,
+          labels: ['Graph', 'Realtime'],
+          radiusStyle: true,
+          onToggle: (index) {
+            if (index == 0) {
+              Navigator.push(
+                context,
+                //graph
+                MaterialPageRoute(builder: (context) => SoilECgraph()),
+              );
+            } else if (index == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SoilEcValue()),
+              );
+            }
+          },
+        ),
+        SizedBox(height: 0),
+        Expanded(
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.width * 0.9,
+              padding: EdgeInsets.all(12),
+              child: KdGaugeView(
+                minSpeed: 0,
+                maxSpeed: 100,
+                speed: speedValue.toDouble(),
+                animate: true,
+                duration: Duration(seconds: 5),
+                alertSpeedArray: [40, 80, 90],
+                alertColorArray: [Colors.green, Colors.indigo, Colors.green],
+                unitOfMeasurement: "",
+                gaugeWidth: 20,
+                fractionDigits: 0,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: SoilEcValue(),
+  ));
+}
